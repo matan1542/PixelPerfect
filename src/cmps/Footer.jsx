5f5c2440f5624282fa4aa73abd7c391b3ff6c870
matan1542 <matan8380@gmail.com>
2021-06-16 19:12:23 +0300

    responsive

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -2,7 +2,33 @@
 export function Footer() {
     return (
         <div className="footer-container">
-            
+            <div className="logo-footer-container">
+               <h1 className="logo">myTeam</h1>
+            </div>
+            <div className="footer-href">
+                <ul className="footer-list clean-list flex">
+                    <li>home</li>
+                    <li>about</li>
+                </ul>
+            </div>
+            <div className="address-content">
+                <ul className="address-list clean-list flex column">
+                    <li>987  Hillcrest Lane</li>
+                    <li>Irvine, CA</li>
+                    <li>California 92714</li>
+                    <li>Call Us : 949-833-7432</li>
+                </ul>
+            </div>
+            <div className="social-content">
+                <ul className="social-list clean-list flex ">
+                    <li><img src="imgs/facebook.png" alt="img" /></li>
+                    <li><img src="imgs/pinterest.png" alt="img" /></li>
+                    <li><img src="imgs/twitter.png" alt="img" /></li>
+                </ul>
+            </div>
+            <div className="copyright-content">
+                <h4>Copyright 2020. All Rights Reserved</h4>
+            </div>
         </div>
     )
 }
--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useEffect, useState } from "react";
+import { useLayoutEffect } from "react";
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
 export function Header() {
 const [toggle,setToggle] = useState(false);
-const matches = useMediaQuery('(min-width:800px)');
+const [width, height] = useWindowSize();
+let matches =(width<800)?false:true;
+useEffect(() =>{
+if(matches && toggle){
+  setToggle(false)
+}
+},[matches])
+const toggleMenu = ()=>{
+  setToggle(!toggle);
+}
   return (
   <div className="header">
  
       <div className="navbar-header ">
-        <div className="logo-side flex">
           <h1 className="logo">myTeam</h1>
+          {((matches && !toggle )||( toggle && !matches) || matches)&&
+          <div className={`navbar-list-href flex `}>
+          <div className="left-side flex">
           <ul className="nav-list clean-list flex">
             <li>home</li>
             <li>about</li>
@@ -18,6 +42,8 @@ const matches = useMediaQuery('(min-width:800px)');
         <div className="nav-right-side">
           <button className="contact-btn">Contact us</button>
         </div>
+          </div>}
+          {!matches && <button onClick={toggleMenu} className="btn btn-open-menu"><i className="fas fa-bars"></i></button>}
       </div>
 
       <div className="header-container">
